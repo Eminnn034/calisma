9d9a9e70171ef4ca37d0e1a72fe42236824a0a5e
Eminnn034 <emin.seyid97@gmail.com>
2024-12-16 19:08:24 +0400

    calisma1

---
+++ app.js
@@ -0,0 +1,8 @@
+function addStudents(params) {
+    let parentslist = document.querySelector("li");
+
+    let newParents = document.createElement("li");
+    newParents.textContent = name;
+    parentslist.appendstudentd(newParents);
+}
+addStudents('Ayxan Eliyev');
